c404268c535fc2fac303f80fe7e285895d435a91
Tanuka Das <das.tanuka@yahoo.com>
2019-04-01 13:09:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 
 // Add your changeCompletely() function here:
+
+
+//Callbacks are a very important concept that you'll employ throughout your JavaScript career.
+//The basic idea is that we define a function but delegate control of when it should be invoked to another function.
