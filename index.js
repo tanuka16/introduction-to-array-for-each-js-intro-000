8f4f6ca65d6bfef7a25f92645be23c458d280eeb
Tanuka Das <das.tanuka@yahoo.com>
2019-04-01 13:12:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Add your doToElementsInArray() function here:
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+// log out animals -- pretty cool, right?
+console.log(animals);
+
+//Callbacks are a very important concept that you'll employ throughout your JavaScript career.
+//The basic idea is that we define a function but delegate control of when it should be invoked to another function.
